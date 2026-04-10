88dee17e3dbdfa38bc6702af2abcb01d0cde6b3e
unknown <tiagosilva0547@digitalcollege.internal>
2026-04-09 21:55:34 -0300

    Adicionando CSS

---
+++ src/components/CardList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CardList = () => {
+
+    const getMoviews = async () =>{
+        
+    }
+  return (
+    <div>CardList</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { Footer } from '../components/Footer'
 export const Home = () => {
   return (
 
-
     <div>
       Home
     </div>
